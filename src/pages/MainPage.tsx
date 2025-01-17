b0198fa2d745b15f1c40e417d3d602d97088c5bb
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2025-01-17 22:12:41 +0900

    design: 메인페이지 레이아웃 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button } from 'react-bootstrap';
+import { Button, Image } from 'react-bootstrap';
 
-import MongguImg from '../assets/monggu.png';
+import MongguImg from '../assets/monggu2.png';
 import { useNavigate } from 'react-router-dom';
 
 function MainPage(): React.ReactElement {
@@ -15,16 +15,18 @@ function MainPage(): React.ReactElement {
   return (
     <>
       <Wrapper>
-        <Header>헤더</Header>
-        <Title>나에게 맞는 집사는?</Title>
-        <LogoImg>
-          <img src={MongguImg} width={350} height={350} />
-        </LogoImg>
-        <Desc>MBTI를 기반으로 하는 나랑 잘 맞는 고양이 찾기!</Desc>
-        <Desc>내가 집사가 되서 고양이를 키운다면?</Desc>
-        <Button className="btn-danger" onClick={handleClickButton}>
-          테스트 시작하기
-        </Button>
+        <Header>😸 예비 집사 판별기 😸</Header>
+        <ContentsWrapper>
+          <Title>나에게 맞는 집사는?</Title>
+          <LogoImg>
+            <Image className="rounded-circle" src={MongguImg} width={350} height={350} />
+          </LogoImg>
+          <Desc>MBTI를 기반으로 하는 나랑 잘 맞는 고양이 찾기!</Desc>
+          <Desc>내가 집사가 되서 고양이를 키운다면?</Desc>
+          <Button className="btn-primary" onClick={handleClickButton} style={{ fontSize: 25, marginTop: 40, marginBottom: 40 }}>
+            테스트 시작하기
+          </Button>
+        </ContentsWrapper>
       </Wrapper>
     </>
   );
@@ -33,23 +35,41 @@ function MainPage(): React.ReactElement {
 export default MainPage;
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100vh;
-  background: #fff;
+  background: #cdf5ff;
 `;
 
 const Header = styled.div`
-  font-size: 50px;
-  color: red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80px;
+  font-size: 40px;
+  background: #7ac4ff;
+`;
+
+const ContentsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 30px;
 `;
 
 const Title = styled.div`
-  font-size: 50px;
-  color: red;
+  font-size: 30px;
+  margin-top: 30px;
 `;
 
-const LogoImg = styled.div``;
+const LogoImg = styled.div`
+  width: 350px;
+  height: 350px;
+  margin: 30px 0;
+`;
+
 const Desc = styled.div`
-  font-size: 50px;
-  color: red;
+  font-size: 20px;
 `;
