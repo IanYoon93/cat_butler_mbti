c877496470369212ce58d944419347f5d5b9f0a8
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2025-01-17 20:56:26 +0900

    docs: App.css 주석 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import './App.css';
+// import './App.css';
 import Router from './router/Router';
 
 function App(): React.ReactElement {
