62c43fefdc29662a22450d2f2af47153f3d1602a
IanYoon93 <yoonkyoungmi.ian@gmail.com>
2025-01-17 21:05:18 +0900

    feat: 메인페이지 구조 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Button } from 'react-bootstrap';
+
+import MongguImg from '../assets/monggu.png';
 
 function MainPage(): React.ReactElement {
-  return <div className="read-the-docs">이곳은 메인페이지</div>;
+  return (
+    <>
+      <Wrapper>
+        <Header>헤더</Header>
+        <Title>나에게 맞는 집사는?</Title>
+        <LogoImg>
+          <img src={MongguImg} width={350} height={350} />
+        </LogoImg>
+        <Desc>MBTI를 기반으로 하는 나랑 잘 맞는 고양이 찾기!</Desc>
+        <Desc>내가 집사가 되서 고양이를 키운다면?</Desc>
+        <Button className="btn-danger">테스트 시작하기</Button>
+      </Wrapper>
+    </>
+  );
 }
 
 export default MainPage;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: #fff;
+`;
+
+const Header = styled.div`
+  font-size: 50px;
+  color: red;
+`;
+
+const Title = styled.div`
+  font-size: 50px;
+  color: red;
+`;
+
+const LogoImg = styled.div``;
+const Desc = styled.div`
+  font-size: 50px;
+  color: red;
+`;
